5f0610ca59a44d88d710cb73792c4e49e444c5af
Ocquaye Smart <snaocquaye@st.knust.edu.gh>
2026-06-20 22:04:16 -0100

    Stage one completed.

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        int number;
+        String number;
         int pinOriginal = 0;
         int pinEntered;
-        double balance = 0;
+        double balance = 150.00;
         int input;
         double send_amount;
         boolean isRunning = true;
         System.out.print("Enter your phone number: ");
-        number = scanner.nextInt();
+        number = scanner.nextLine();
         System.out.print("Enter your pin: ");
         pinEntered = scanner.nextInt();
         if(pinEntered == pinOriginal){
@@ -27,21 +27,21 @@ public class Main {
                     case 2 -> {
                         System.out.print("Enter an amount: ");
                         send_amount = scanner.nextDouble();
-                        if(balance > send_amount){
+                        if(balance >= send_amount){
                             balance = balance - send_amount;
                             System.out.printf("Sent GHS%.02f\n" ,send_amount);
                         }
-                        else if(balance < send_amount){
-                            System.out.println("Insufficient funds\n");
-                        }
                         else{
-                            System.out.println("Invalid Option\n");
+                            System.out.println("Insufficient funds\n");
                         }
                     }
                     case 3 -> isRunning = false;
                     default -> System.out.println("Invalid Option");
                 }
             }
+        }
+        else{
+            System.out.println("Incorrect PIN");
         }
     }
 }
