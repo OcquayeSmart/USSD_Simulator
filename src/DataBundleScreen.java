3686e9e77483e87f546e9c188fdd24e07d14f805
Ocquaye Smart <snaocquaye@st.knust.edu.gh>
2026-06-23 18:02:22 -0100

    Honestly forgot to print out the databundles themselves before the user can select. So I used a basic for loop here to implement that functionality. Also included a counter for each data bundle option.

--- src/DataBundleScreen.java
+++ src/DataBundleScreen.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DataBundleScreen implements MenuScreen{
-    DataBundle dataBundle;
+    int counter = 1;
     List<DataBundle> bundles = new ArrayList<>(){{
        add(new DataBundle(2.45, 20, 30));
         add(new DataBundle(6.13, 50, 30));
@@ -13,14 +13,15 @@ public class DataBundleScreen implements MenuScreen{
     }};
     @Override
     public void render(Account account) {
+        for(DataBundle bundle : bundles){
+            System.out.println(counter + ". " + bundle.describe());
+            counter++;
+        }
         System.out.println("Enter your choice: ");
     }
 
     @Override
     public MenuScreen handleInput(String input, Account account) {
-        int bundleChoice = Integer.parseInt(input);
-        DataBundle myBundle = bundles.get(bundleChoice);
-        dataBundle.describe(myBundle);
         return new MainMenuScreen();
     }
 }
