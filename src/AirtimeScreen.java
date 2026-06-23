524c118523a37f43e0facc1734bc4654cf082402
Ocquaye Smart <snaocquaye@st.knust.edu.gh>
2026-06-23 17:58:37 -0100

    Added checks for the amount of airtime a user can purchase depending on their balance. Included parsing input in a try catch block to catch invalid inputs by the user.

--- src/AirtimeScreen.java
+++ src/AirtimeScreen.java
@@ -7,10 +7,24 @@ public class AirtimeScreen implements MenuScreen{
 
     @Override
     public MenuScreen handleInput(String input, Account account) {
-        double amount = Double.parseDouble(input);
-        double user_balance = account.getBalance();
-        account.setBalance(user_balance - amount);
-        System.out.printf("Congratulations! You have received GHS%.02f of airtime", amount);
+        try{
+            double amount = Double.parseDouble(input);
+            double user_balance = account.getBalance();
+            if(amount <= account.getBalance()){
+                account.setBalance(user_balance - amount);
+                System.out.printf("Congratulations! You have received GHS%.02f of airtime", amount);
+            }
+            else{
+                System.out.println("Insufficient funds");
+                return new AirtimeScreen();
+            }
+        }
+        catch(NumberFormatException e){
+            System.out.println("Invalid Input");
+        }
+        catch(Exception e){
+            System.out.println("An error occurred");
+        }
         return new MainMenuScreen();
     }
 }
