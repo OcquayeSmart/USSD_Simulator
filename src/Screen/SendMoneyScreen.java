43720deac17bbf3e54299fb198a0d8a7c491c6b7
Ocquaye Smart <snaocquaye@st.knust.edu.gh>
2026-06-30 21:56:09 -0100

    In the SendMoneyScreen, implementing the transfer of funds to other accounts.

--- src/Screen/SendMoneyScreen.java
+++ src/Screen/SendMoneyScreen.java
@@ -3,32 +3,62 @@ package Screen;
 import core.MenuScreen;
 import model.Account;
 
+import java.util.Map;
+
 public class SendMoneyScreen implements MenuScreen {
+    private final Map<String, Account> accounts;
+    private Step step = Step.RECIPIENT;
+    private String recipientNumber;
+
+    public SendMoneyScreen(Map<String, Account> accounts) {
+        this.accounts = accounts;
+    }
     @Override
     public void render(Account account) {
-        System.out.print("Enter an amount(GHS): \n");
+        if(step == Step.RECIPIENT){
+            System.out.print("\nEnter recipient's phone number: ");
+            System.out.println();
+        }
+        else{
+            System.out.print("\nEnter an amount(GHS): ");
+            System.out.println();
+        }
     }
 
     @Override
     public MenuScreen handleInput(String input, Account account) {
-        try{
-            double amountSent = Double.parseDouble(input);
-            if(amountSent <= account.getBalance()){
-                double current_balance = account.getBalance();
-                account.setBalance(current_balance - amountSent);
-                System.out.printf("You have sent GHS%.02f", amountSent);
-
-            } else{
-                System.out.println("Insufficient funds");
-                return new SendMoneyScreen();
+        if(step == Step.RECIPIENT){
+            recipientNumber = input;
+            if(accounts.containsKey(recipientNumber)){
+                System.out.println("Send to: " + accounts.get(recipientNumber));
+                step = Step.AMOUNT;
+            }
+            else{
+                System.out.println("Account (" + accounts.get(recipientNumber) + ")" + " not found");
+                System.out.println("Transaction Failed");
+                return new MainMenuScreen(accounts);
             }
         }
-        catch(NumberFormatException e){
-            System.out.println("Invalid Input");
+        else if(step == Step.AMOUNT){
+            try{
+                double amountSent = Double.parseDouble(input);
+                if(amountSent <= account.getBalance()){
+                    double current_balance = account.getBalance();
+                    account.setBalance(current_balance - amountSent);
+                    System.out.printf("\nSent GHS%.02f to %s", amountSent, accounts.get(recipientNumber));
+                    account.setBalance(account.getBalance() + amountSent);
+                } else{
+                    System.out.println("Insufficient funds");
+                    return new SendMoneyScreen(accounts);
+                }
+            }
+            catch(NumberFormatException e){
+                System.out.println("Invalid Input");
+            }
+            catch(Exception e){
+                System.out.println("An error occurred");
+            }
         }
-        catch(Exception e){
-            System.out.println("An error occurred");
-        }
-        return new MainMenuScreen();
+        return new MainMenuScreen(accounts);
     }
 }
